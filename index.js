70575c2ccb5d73200f8b9f87f434dd9ee9ac3b3f
kgtkr <contact@kgtkr.net>
2022-02-20 22:12:48 +0900

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const name = core.getInput("name");
+const tags = [];
+tags.push(github.context.sha);
+if (github.context.ref.startWith("refs/tags/")) {
+  const ref = github.context.ref.replace("refs/tags/", "");
+  tags.push(ref.replaceAll("/", "-"));
+}
+if (github.context.ref.startWith("refs/heads/")) {
+  const ref = github.context.ref.replace("refs/heads/", "");
+  tags.push(ref.replaceAll("/", "-"));
+}
+core.setOutput("tags", tags.map((tag) => `${name}:${tag}`).join(","));
