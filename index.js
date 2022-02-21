ccec55d1d4cbc72ccf1bda0d15b3d8ac72403b25
kgtkr <contact@kgtkr.net>
2022-02-21 20:14:05 +0900

    ロジック変更

--- index.js
+++ index.js
@@ -9,23 +9,29 @@ const tags = [];
 let ref = "_unknown_";
 if (github.context.ref.startsWith("refs/tags/")) {
   ref = convertRef(github.context.ref.replace("refs/tags/", ""));
+  tags.push(ref);
 }
 if (github.context.ref.startsWith("refs/heads/")) {
   ref = convertRef(github.context.ref.replace("refs/heads/", ""));
-}
-tags.push(ref);
+  tags.push(`latest-${ref}`);
 
-const now = new Date();
-const timestamp =
-  now.getUTCFullYear().toString().padStart(4, "0") +
-  (now.getUTCMonth() + 1).toString().padStart(2, "0") +
-  now.getUTCDate().toString().padStart(2, "0") +
-  now.getUTCHours().toString().padStart(2, "0") +
-  now.getUTCMinutes().toString().padStart(2, "0") +
-  now.getUTCSeconds().toString().padStart(2, "0") +
-  now.getUTCMilliseconds().toString().padStart(3, "0");
-const shortSha = github.context.sha.substring(0, 7);
-tags.push(`${timestamp}-${ref}-${shortSha}`);
+  const now = new Date();
+  const timestamp =
+    now.getUTCFullYear().toString().padStart(4, "0") +
+    (now.getUTCMonth() + 1).toString().padStart(2, "0") +
+    now.getUTCDate().toString().padStart(2, "0") +
+    now.getUTCHours().toString().padStart(2, "0") +
+    now.getUTCMinutes().toString().padStart(2, "0") +
+    now.getUTCSeconds().toString().padStart(2, "0") +
+    now.getUTCMilliseconds().toString().padStart(3, "0");
+  const shortSha = github.context.sha.substring(0, 7);
+  if (ref === "master" || ref === "main") {
+    tags.push(`${timestamp}-${shortSha}`);
+  } else {
+    tags.push(`${timestamp}-${shortSha}-${ref}`);
+  }
+}
+
 const result = tags.map((tag) => `${name}:${tag}`).join(",");
 console.log("Generated tags:", result);
 core.setOutput("tags", result);
