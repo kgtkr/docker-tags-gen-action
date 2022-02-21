6bf7076c9240b8bdd94fe3ff6936fc86693ebf58
kgtkr <contact@kgtkr.net>
2022-02-21 19:53:24 +0900

    タグ付けルールを変更、タイムスタンプを入れるようにした

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+const convertRef = (ref) =>
+  ref.substring(0, 64).replaceAll(/[^a-zA-Z0-9_\.]|^\.|\.$/g, "_");
+
 const name = core.getInput("name");
 const tags = [];
-tags.push(github.context.sha);
+let ref = "_unknown_";
 if (github.context.ref.startsWith("refs/tags/")) {
-  const ref = github.context.ref.replace("refs/tags/", "");
-  tags.push(ref.replaceAll("/", "-"));
+  ref = convertRef(github.context.ref.replace("refs/tags/", ""));
 }
 if (github.context.ref.startsWith("refs/heads/")) {
-  const ref = github.context.ref.replace("refs/heads/", "");
-  tags.push(ref.replaceAll("/", "-"));
+  ref = convertRef(github.context.ref.replace("refs/heads/", ""));
 }
+tags.push(ref);
+
+const now = new Date();
+const timestamp =
+  now.getUTCFullYear().toString().leftPad(4, "0") +
+  (now.getUTCMonth() + 1).toString().leftPad(2, "0") +
+  now.getUTCDate().toString().leftPad(2, "0") +
+  now.getUTCHours().toString().leftPad(2, "0") +
+  now.getUTCMinutes().toString().leftPad(2, "0") +
+  now.getUTCSeconds().toString().leftPad(2, "0") +
+  now.getUTCMilliseconds().toString().leftPad(3, "0");
+const shortSha = github.context.sha.substring(0, 7);
+tags.push(`${ref}-${shortSha}-${timestamp}`);
 core.setOutput("tags", tags.map((tag) => `${name}:${tag}`).join(","));
